adc125e9d95c7cb4b62b675e20a789bb2f4796b2
carlos0716 <chulezyc@gmail.com>
2023-01-05 20:09:06 -0600

    Adds to the divide function in case the dividend or the divisor are equal to zero.

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ const substract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
 
-const divide = (a, b) => a / b;
+const divide = (a, b) => {
+    if (a === 0){
+        return 0;
+    }
+    else if( b === 0) {
+        return console.log('Error');
+    }
+    else {
+        a / b
+    }
+};
 
 
 
